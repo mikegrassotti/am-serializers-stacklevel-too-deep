a6f7a245d2b229bad2a46cc810b44bedd776f37c
Mike Grassotti <mgrassotti@gmail.com>
2015-02-01 21:52:01 -0500

    add meta info to the posts api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    render json: @posts
+    render json: @posts,  meta: {total: Post.count}
   end
   def show
     @post = Post.find(params[:id])
